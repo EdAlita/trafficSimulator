8a645c51333b1e9203a50fda00e6630d3c84b784
Edwing Ulin <eulinbriseno@gmail.com>
2022-12-08 14:06:33 +0100

    Updates to pedestrain object

--- pedestrain.py
+++ pedestrain.py
@@ -34,12 +34,44 @@ class Pedestrain:
     
     def update(self, lead, dt):
         #update position and velocity
-
+        if self.v + self.a*dt < 0:
+            self.x -= 1/2*self.v*self.v/self.a
+            self.v = 0
+        else: 
+            self.v += self.a*dt
+            self.x += self.v*dt + self.a*dt*dt/2     
+          
         #update acceleration
+        alpha = 0
+        if lead:
+            delta_x = lead.x - self.x -lead.l
+            delta_v = self.v - lead.v
 
+            alpha = (self.s0 + max(0, self.T*self.v + delta_v*self.v/self.sqrt_ab) ) / delta_x
+
+            self.a = self.a_max * (1-(self.v/self.v_max)**4 - alpha**2)
+
+        if self.stopped:
+            if self.rougue==False:
+                self.a = -self.b_max*self.v/self.v_max    
+            
         #Stop and Latin America funtion
 
-        return
+    def stop(self):
+        self.stopped = True
 
+    def unstop(self):
+        self.stopped = False
 
+    def slow(self, v):
+        self.v_max = v
+
+    def unslow(self):
+        self.v_max = self._v_max
+    
+    def latinModeOFF(self):
+        self.rougue=False
+    
+    def latinModeOn(self):
+        self.rougue=True
         
