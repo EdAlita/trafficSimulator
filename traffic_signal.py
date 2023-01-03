ca622d17f7b8f07efb154919675af40a5eb1fb9b
Edwing Ulin <eulinbriseno@gmail.com>
2023-01-03 20:18:05 +0100

    Update code

--- traffic_signal.py
+++ traffic_signal.py
@@ -1,14 +1,13 @@
 class TrafficSignal:
     def __init__(self, roads, config={}):
-        #Initialize roads
+        # Initialize roads
         self.roads = roads
-        #Set default configuration
+        # Set default configuration
         self.set_default_config()
-        #Update configuration
+        # Update configuration
         for attr, val in config.items():
             setattr(self, attr, val)
-        
-        #Calculate properties
+        # Calculate properties
         self.init_properties()
 
     def set_default_config(self):
@@ -24,10 +23,9 @@ class TrafficSignal:
     def init_properties(self):
         for i in range(len(self.roads)):
             for road in self.roads[i]:
-                road.set_traffic_signal(self,i)
-    
-    @property
+                road.set_traffic_signal(self, i)
 
+    @property
     def current_cycle(self):
         return self.cycle[self.current_cycle_index]
     
--- vehicle.py
+++ vehicle.py
@@ -1,18 +1,18 @@
 import numpy as np
 
-class Vehicle: 
+class Vehicle:
     def __init__(self, config={}):
-        #Set default configuration 
+        # Set default configuration
         self.set_default_config()
 
-        #Update configuration
-        for attr, val, in config.items():
+        # Update configuration
+        for attr, val in config.items():
             setattr(self, attr, val)
-        
-        #Calculate properties
+
+        # Calculate properties
         self.init_properties()
-    
-    def set_default_config(self):
+
+    def set_default_config(self):    
         self.l = 4
         self.s0 = 4
         self.T = 1
@@ -21,43 +21,41 @@ class Vehicle:
         self.b_max = 4.61
 
         self.path = []
-
         self.current_road_index = 0
 
         self.x = 0
         self.v = self.v_max
         self.a = 0
         self.stopped = False
-        self.rougue = False
-    
+        self.latinModeOn = False
+
     def init_properties(self):
-        self.sqrt_ab = 2*np.sqrt(self.a*self.b_max)
+        self.sqrt_ab = 2*np.sqrt(self.a_max*self.b_max)
         self._v_max = self.v_max
 
     def update(self, lead, dt):
-        #update position and velocity
+        # Update position and velocity
         if self.v + self.a*dt < 0:
             self.x -= 1/2*self.v*self.v/self.a
             self.v = 0
-        else: 
+        else:
             self.v += self.a*dt
             self.x += self.v*dt + self.a*dt*dt/2
         
-        #update acceleration
+        # Update acceleration
         alpha = 0
-
-        if lead: 
-            delta_x = lead.x - self.x -lead.l
+        if lead:
+            delta_x = lead.x - self.x - lead.l
             delta_v = self.v - lead.v
 
-            alpha = (self.s0 + max(0, self.T*self.v + delta_v*self.v/self.sqrt_ab) ) / delta_x
+            alpha = (self.s0 + max(0, self.T*self.v + delta_v*self.v/self.sqrt_ab)) / delta_x
 
         self.a = self.a_max * (1-(self.v/self.v_max)**4 - alpha**2)
 
         if self.stopped:
-            if self.rougue==False:
+            if self.latinModeOn == False: 
                 self.a = -self.b_max*self.v/self.v_max
-
+        
     def stop(self):
         self.stopped = True
 
@@ -69,9 +67,9 @@ class Vehicle:
 
     def unslow(self):
         self.v_max = self._v_max
+
+    def latinmode(self):
+            self.latinModeOn=True
     
-    def latinModeOFF(self):
-        self.rougue=False
-    
-    def latinModeOn(self):
-        self.rougue=True
+    def normalmode(self):
+            self.latinmode=False
